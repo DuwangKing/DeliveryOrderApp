b1a6b7b70cd11191b89ef900962aa1499bb509c2
DuwangKing <arturius07@mail.ru>
2026-09-24 14:12:27 +0300

    feat: add DTO order model

---
+++ DTOs/CreateOrderDto.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DeliveryOrderApp.DTOs
+{
+    public class CreateOrderDto
+    {
+        [Required(ErrorMessage = "Город отправителя обязателен")]
+        [Display(Name = "Город отправителя")]
+        public string SenderCity { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Адрес отправителя обязателен")]
+        [Display(Name = "Адрес отправителя")]
+        public string SenderAdress { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Город получателя обязателен")]
+        [Display(Name = "Город получателя")]
+        public string RecipientCity { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Адрес получателя обязателен")]
+        [Display(Name = "Адрес получателя")]
+        public string RecipientAdress { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Вес груза обязателен")]
+        [Range(0.1, 100000, ErrorMessage = "Вес должен быть больше 0 и меньше 100000")]
+        [Display(Name = "Вес груза (кг)")]
+        public decimal Weight { get; set; }
+
+        [Required(ErrorMessage = "Дата забора груза обязательна")]
+        [DataType(DataType.Date)]
+        [Display(Name = "Дата забора груза")]
+        public DateTime PickupDate { get; set; }
+    }
+}
